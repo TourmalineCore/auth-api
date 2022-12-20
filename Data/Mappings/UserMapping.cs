87e9d93cb26ad16b2db33c40069c1b31bd356d70
AnyaNek0 <16bitstreams@gmail.com>
2022-12-20 16:13:56 +0500

    feat: added default user

---
+++ Data/Mappings/UserMapping.cs
@@ -0,0 +1,28 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Runtime.ConstrainedExecution;
+
+namespace Data.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(new { Id = 1L, 
+                IsBlocked = false, 
+                UserName = "ceo@tourmalinecore.com",
+                NormalizedUserName = "CEO@TOURMALINECORE.COM",
+                EmailConfirmed = false,
+                PasswordHash = "AQAAAAEAACcQAAAAEEim42ocT0sL9H818pD8yMxUDtA3GUCe+SDwlxl0geUX56G2GEvu7G6DKHjXMHowlA==",
+                SecurityStamp = "LCZLJS5K7YP2FO2NFI2P6CQZVJLWEYZZ",
+                ConcurrencyStamp = "15cfcf9e-94f3-4b65-884c-e03f1588fa20",
+                PhoneNumberConfirmed = false,
+                TwoFactorEnabled = false,
+                LockoutEnabled = true,
+                AccessFailedCount = 0,
+                AccountId = 1L
+            });
+        }
+    }
+}
---
+++ Data/Migrations/20221220110943_AddDefaultCeo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class AddDefaultCeo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "AccountId", "ConcurrencyStamp", "Email", "EmailConfirmed", "IsBlocked", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { 1L, 0, 1L, "15cfcf9e-94f3-4b65-884c-e03f1588fa20", null, false, false, true, null, null, "CEO@TOURMALINECORE.COM", "AQAAAAEAACcQAAAAEEim42ocT0sL9H818pD8yMxUDtA3GUCe+SDwlxl0geUX56G2GEvu7G6DKHjXMHowlA==", null, false, "LCZLJS5K7YP2FO2NFI2P6CQZVJLWEYZZ", false, "ceo@tourmalinecore.com" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1L);
+        }
+    }
+}
