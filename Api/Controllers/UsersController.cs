934be5f4eba2375b35c913b39fea61384a5e10d7
Anna <141008173+Annushkaaaaa@users.noreply.github.com>
2024-10-25 15:34:41 +0500

    chore: replay exception

--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -21,6 +21,7 @@ namespace Api.Controllers
         }
 
         [Authorize]
+        [RequiresPermission(UserClaimsProvider.CanManageAccounts)]
         [HttpPost("register")]
         public async Task<ActionResult> RegisterUserAsync([FromBody] RegistrationModel registrationModel)
         {
