48c0ada1988f3256a7e0eaf3a059979995d04e77
akovylyaeva <akovylyaeva@tourmalinecore.com>
2025-09-12 14:45:03 +0500

    chore: change employee model

--- DataAccess/Models/Employee.cs
+++ DataAccess/Models/Employee.cs
@@ -3,10 +3,4 @@ namespace DataAccess.Models;
 public class Employee
 {
     public long Id { get; set; }
-
-    public string FullName { get; set; }
-
-    public string CorporateEmail { get; set; }
-
-    public long TenantId { get; set; }
 }
